0a08c075695a04bf1bae149fd7666af48fcd8b4d
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2020-04-16 12:05:22 +1200

    statement generater

--- 07_HL_statement_generator.py
+++ 07_HL_statement_generator.py
@@ -8,14 +8,14 @@ def h1_statement(statement, char):
     print
 
 # Main routine
-too_low = h1_statement("^^ Too low, tey a higher number.   "
-                       "Guesses Left: 3 ^^" "^")
+too_low = h1_statement("^^ Too low, try a higher number.  |  "
+                       "Guesses Left: 3 ^^", "^")
 print()
-too_high = h1_statement("vv Too high, try a lower number.    "
-                         "Guesses Left: 2 vv" "v")
+too_high = h1_statement("vv Too high, try a lower number.  |   "
+                         "Guesses Left: 2 vv", "v")
 print()
-duplicate - h1_statement(" !! You already guessed that # Please try again.   "
-                        "Guesses Lefts: 2 !!" "!" )
+duplicate = h1_statement(" !! You already guessed that # Please try again. |  "
+                        "Guesses Lefts: 2 !!", "!" )
 print()
 well_done = h1_statement("*** Well done! You got it in 3 guesses ***", "*" )
 
